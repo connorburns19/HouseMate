5097da24456ecc56fafac2d73e4c23d9064b06c5
ekluthra <ekluthra@yahoo.com>
2021-10-27 14:28:24 -0400

    created basic columns

--- house_mate/src/App.js
+++ house_mate/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Lets Gooooooo
-        </a>
-      </header>
+      <div className="Row">
+        <div className="Column">
+          <div className = "LeftColumn">lefthand</div>
+        </div>
+        <div className="Column">
+        <div className = "MidColumn">middle</div>
+        </div>
+        <div className="Column">
+        <div className = "RightColumn">righthand</div>
+        </div>
+      </div>
     </div>
   );
 }

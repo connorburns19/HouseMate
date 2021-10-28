31ddacb100da0a357542f70ebea6752579054cbc
ekluthra <ekluthra@yahoo.com>
2021-10-28 02:21:23 -0400

    added feature to hide and show details of expenses upon mousehover

--- house_mate/src/App.js
+++ house_mate/src/App.js
@@ -1,19 +1,36 @@
 import './App.css';
+import { useState } from 'react';
 import NavBar from './NavBar';
 
 function App() {
+  const [leftDetails, leftDetailsShown] = useState(false);
+
   return (
     <div className="App">
       <NavBar/>
       <div className="Row">
         <div className="Column">
-          <div className = "LeftColumn">lefthand</div>
+          <div 
+          className = "LeftColumn" 
+          onMouseEnter={() => leftDetailsShown(true)}
+          onMouseLeave={() => leftDetailsShown(false)}
+          >
+            {!leftDetails && (
+            <h1>View Expenses</h1>
+            )}
+            
+            {leftDetails && (
+              <h1>details</h1>
+            )}
+
+          </div>
+          
         </div>
         <div className="Column">
-        <div className = "MidColumn">middle</div>
+        <div className = "MidColumn"><h1>Add Expenses</h1></div>
         </div>
         <div className="Column">
-        <div className = "RightColumn">righthand</div>
+        <div className = "RightColumn"><h1>House Rules</h1></div>
         </div>
       </div>
     </div>
--- house_mate/src/NavBar.js
+++ house_mate/src/NavBar.js
@@ -3,8 +3,8 @@ import './NavBar.css';
 function NavBar() {
     return(
         <header className = "NavBar">
-            <div className= "Address">Street_Address</div>
-            <div className = "Profile">User_Icon</div>
+            <div className= "Address"><h1>40 St George Street</h1></div>
+            <div className = "Profile">Profile</div>
         </header>
     );
 }

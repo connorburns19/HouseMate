b37d6f30271bb7332a9080a9477e7e8e322c0782
ekluthra <ekluthra@yahoo.com>
2021-10-29 14:41:21 -0400

    added onhover for house rules

--- house_mate/src/App.js
+++ house_mate/src/App.js
@@ -5,6 +5,7 @@ import NavBar from './NavBar';
 function App() {
   const [leftDetails, leftDetailsShown] = useState(false);
   const [addExpense, addExpenseShown] = useState(false);
+  const [houseRules, houseRulesShown] = useState(false);
 
   return (
     <div className="App">
@@ -22,8 +23,8 @@ function App() {
             
             {leftDetails && (
               <h1>Details</h1>
-            )}
-
+            )
+          }
           </div>
           
         </div>
@@ -43,7 +44,19 @@ function App() {
         </div>
         </div>
         <div className="Column">
-        <div className = "RightColumn"><h1>House Rules</h1></div>
+        <div 
+        className = "RightColumn"
+        onMouseEnter={() => houseRulesShown(true)}
+        onMouseLeave={() => houseRulesShown(false)}
+        >
+          {!houseRules && (
+            <h1>House Rules</h1>
+          )}
+          {houseRules && (
+            <h1>Payments are to be made on the 1st of each month</h1>
+          )}
+          
+        </div>
         </div>
       </div>
     </div>

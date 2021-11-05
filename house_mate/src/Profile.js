f446dd348141415ae47a03e94f0afecdbc59ce2d
Connor Burns <connor.burns@mail.utoronto.ca>
2021-11-05 17:03:29 -0400

    small changes to profile page

--- house_mate/src/Profile.js
+++ house_mate/src/Profile.js
@@ -8,32 +8,32 @@ import Button from '@mui/material/Button';
 
 function Profile() {
     return(
-        <body>
-        <header className = "NavBar">
-            <div className= "Address">My Profile</div>
-        </header>
-        <div class='ImageCommentContainer'>
-        <div> 
-            <img src='https://heightzone.com/wp-content/uploads/2020/08/e4c130396c1c9886e8a6d9962c3a9d.jpg' class='Image'></img>
-        </div>
-        <div class='Comment'>
-            I like pizza and programming. I also like pizza and programming. I also like pizza and programming. I also like pizza and programming. I also like pizza and programming. I also like pizza and programming. I also like pizza and programming.
-                        <Button variant="contained" size ="small">change</Button>
-        </div>
-        </div>
-        
-        <div class='InfoContainer'>
-        <div class ='DisplayName'> 
-            Connor Burns <Button variant="contained" size ="small">change</Button>
+        <div className="profile-page profilepage--dark">
+            <header className = "NavBar">
+                <div className= "Address">My Profile</div>
+            </header>
+            <div class='ImageCommentContainer'>
+            <div> 
+                <img src='https://heightzone.com/wp-content/uploads/2020/08/e4c130396c1c9886e8a6d9962c3a9d.jpg' class='Image'></img>
             </div>
-        connor.burns@mail.utoronto.ca <Button variant="contained" size ="small">change</Button><br></br>
-        905-396-6805 <Button variant="contained" size ="small">change</Button>
+            <div class='Comment'>
+                I like pizza and programming. I also like pizza and programming. I also like pizza and programming. I also like pizza and programming. I also like pizza and programming. I also like pizza and programming. I also like pizza and programming.
+                            <Button variant="contained" size ="small">change</Button>
+            </div>
+            </div>
+            
+            <div class='InfoContainer'>
+            <div class ='DisplayName'> 
+                Connor Burns <Button variant="contained" size ="small">change</Button>
+                </div>
+            connor.burns@mail.utoronto.ca <Button variant="contained" size ="small">change</Button><br></br>
+            905-396-6805 <Button variant="contained" size ="small">change</Button>
+            </div>
+        
         </div>
-        
-        
         
 
-        </body>
+        
     );
 }
 

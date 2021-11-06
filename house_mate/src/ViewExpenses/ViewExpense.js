0223c7729ccbe822b4cbce4133dc77c00622cd1c
ekluthra <ekluthra@yahoo.com>
2021-11-06 00:27:21 -0400

    added table to view expenses page

--- house_mate/src/ViewExpenses/ViewExpense.js
+++ house_mate/src/ViewExpenses/ViewExpense.js
@@ -1,19 +1,34 @@
 import NavBar from "../NavBar/NavBar.js";
 import theme from "../Themes/theme.js";
 import { ThemeProvider } from "@mui/material/styles";
+import './ViewExpense.css'
+import { Table, TableCell, TableHead, TableRow } from "@mui/material";
 
 function addExpense(name, amount){
-    
+    return { name, amount };
 }
+
+const rows = [
+    addExpense('Connor Burns', -12.23),
+    addExpense('Nathan DeGoey', 10.50),
+    addExpense('Sal Neri', 24.20),
+]
 
 function ViewExpense(){
     return(
         <div className="main-page main-page--dark">
             <ThemeProvider theme={theme}>
                 <NavBar />
-                <div className="add-expense-wrapper" color="primary">
+                <div className="view-expense-wrapper" color="primary">
 				    <div className="main-page__title title-color--primary"><h1>You Owe $69.42</h1></div>
-
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="center">House Member</TableCell>
+                                <TableCell align="center">Amount Owed</TableCell>
+                            </TableRow>
+                        </TableHead>
+                    </Table>
                 </div>
             </ThemeProvider>
         </div>

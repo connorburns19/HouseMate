65ea98aca8ef348e92b85278677ea6e8ba855dc8
Connor Burns <connor.burns@mail.utoronto.ca>
2021-11-05 20:38:38 -0400

    Fixed naming conventions, removed comment box

--- house_mate/src/Profile.js
+++ house_mate/src/Profile.js
@@ -1,40 +1,50 @@
-import React from 'react';
-import './Profile.css';
-import Button from '@mui/material/Button';
-
-
-
-
+import React from "react";
+import "./Profile.css";
+import Button from "@mui/material/Button";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./Themes/theme.js";
+import NavBar from "./NavBar";
+import { RIEToggle, RIEInput, RIETextArea, RIENumber, RIETags, RIESelect } from 'riek'
+import _ from 'lodash'
 
 function Profile() {
-    return(
-        <div className="profile-page profilepage--dark">
-            <header className = "NavBar">
-                <div className= "Address">My Profile</div>
-            </header>
-            <div class='ImageCommentContainer'>
-            <div> 
-                <img src='https://heightzone.com/wp-content/uploads/2020/08/e4c130396c1c9886e8a6d9962c3a9d.jpg' class='Image'></img>
-            </div>
-            <div class='Comment'>
-                I like pizza and programming. I also like pizza and programming. I also like pizza and programming. I also like pizza and programming. I also like pizza and programming. I also like pizza and programming. I also like pizza and programming.
-                            <Button variant="contained" size ="small">change</Button>
-            </div>
-            </div>
-            
-            <div class='InfoContainer'>
-            <div class ='DisplayName'> 
-                Connor Burns <Button variant="contained" size ="small">change</Button>
-                </div>
-            connor.burns@mail.utoronto.ca <Button variant="contained" size ="small">change</Button><br></br>
-            905-396-6805 <Button variant="contained" size ="small">change</Button>
-            </div>
-        
-        </div>
-        
+  return (
+    <div className="profile-page profile-page--dark">
+      <ThemeProvider theme={theme}>
+        <NavBar />
 
-        
-    );
+        <div className="profile-page__flex-wrapper">
+          <div class="profile-page__image-container">
+              <img
+                src="https://heightzone.com/wp-content/uploads/2020/08/e4c130396c1c9886e8a6d9962c3a9d.jpg"
+                class="profile-page__image"
+                ></img>
+            
+            {/* <div class="profile-page__comment">
+              I like pizza and programming. I also like pizza and programming. I
+              also like pizza and programming. I also like pizza and
+              programming. I also like pizza and programming. I also like pizza
+              and programming. I also like pizza and programming.
+            </div> */}
+            <div class="profile-page__infocontainer">
+            <div class="profile-page__displayname">
+              Connor Burns{" "}
+            </div>
+            connorburns191{" "}
+            <br></br>
+            905-396-6805{" "} 
+            <Button variant="contained">change name/number</Button>
+
+          </div>
+          
+          </div>
+
+          
+        </div>
+
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default Profile;
---
+++ house_mate/src/Themes/theme.js
@@ -0,0 +1,22 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        // main: '#FF5151',
+        main: '#88E0EF',
+      },
+      secondary: {
+        main: '#FF9B6A',
+      },
+      ternary: {
+        main: '#161E54',
+      },
+      navbar: {
+        main: '#FF5151',
+      }
+    },
+  }
+);
+
+export default theme;

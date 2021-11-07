f8e579a1c02931cb3f4d35cbec0f185340b850c8
SalCern <sal.cernaneri@mail.utoronto.ca>
2021-11-07 14:04:07 -0500

    added useState to function components

--- house_mate/src/login-components/SignUpForm/SignUpForm.js
+++ house_mate/src/login-components/SignUpForm/SignUpForm.js
@@ -1,29 +1,35 @@
-
 import React, {useState} from 'react';
-import './UserForm.css'
+import './SignUpForm.css'
 import Button from "@material-ui/core/Button";
 import TextField from '@material-ui/core/TextField';
 
-
-function UserForm(){
+function SignUpForm(){
+  const [name, setName] = useState('');
+  const [phonenum, setPhonenum] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+
   const submitValue = () => {
     const formdetails = {
+        'name'     : name,
+        'phonenum' : phonenum,
         'username' : username,
         'password' : password,
     }
     console.log(formdetails);
   }
+
   return (
       <form>
+        <TextField label="Name" variant="filled" required onChange={event =>setName(event.target.value)} /> <br></br>
+        <TextField label="Phone Number" variant="filled"  required onChange={event =>setPhonenum(event.target.value)}  /> <br></br>
         <TextField label="Username" variant="filled" required onChange={event =>setUsername(event.target.value)} /> <br></br>
         <TextField label="Password" variant="filled" required onChange={event =>setPassword(event.target.value)} />  <br></br>
 
         <div>
           <Button variant="contained" onClick = {submitValue}>
-          Login
+          SignUp
           </Button>
         </div>
 
@@ -32,4 +38,4 @@ function UserForm(){
 
 } 
 
-export default UserForm;
+export default SignUpForm;

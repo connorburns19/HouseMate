1dc1fefd2a1da2679277be94f1a7108fe9ae6c15
ekluthra <ekluthra@yahoo.com>
2021-12-06 00:01:47 -0500

    Fix: added server.js

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
+const express = require('express');
+const app = express()
+
 const path = require('path');
 
-const port = process.env.PORT || 5000;
 var environment = process.env.NODE_ENV || 'production';
 
-if (environment === "production") {
-    app.use(express.static('build'));
-    app.get('*', (req, res) => {
-        req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    })
-}
+const cors = require('cors')
+if (environment !== 'production') { app.use(cors()) }
 
-app.listen(port, (err) => {
-    if (err) return console.log(err);
-    console.log('Server running on port: ', port);
-})
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get("*", (req, res) => {
+    // check for page routes that we expect in the frontend to provide correct status code.
+    const goodPageRoutes = ["/"];
+    if (!goodPageRoutes.includes(req.url)) {
+        // if url not in expected page routes, set status to 404.
+        res.status(404);
+    }
+
+    // send index.html
+    res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});
